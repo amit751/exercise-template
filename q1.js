7615595ae7505b1c2871bcc169d1a87cffd04903
amit <amitoosh.mor@gmail.com>
2021-01-19 21:34:05 +0200

    update q1

--- q1.js
+++ q1.js
@@ -1,11 +1,19 @@
 function run() {
+    const n = document.getElementById("n3").value;
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
+    // let out;
+    
+    const inputArr = JSON.parse(input);
+    let temp= [];
+    for( let i=0 ; i<n ; i++){
+        temp.push(inputArr[i]);
+       
+        // out = JSON.stringify(inputArr[i]);
+        // console.log(out);
+    }
+    out=temp;
 
-/**/
     output.innerText = out;
 }
