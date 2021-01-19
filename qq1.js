e33f2e59c4347ccd5667cf9e95b3f3c230f1f216
amit <amitoosh.mor@gmail.com>
2021-01-19 13:17:51 +0200

    qq1
    q1

---
+++ qq1.js
@@ -0,0 +1,22 @@
+function run() {
+    const input = document.getElementById("input").value;
+    const output = document.getElementById("output");
+    let out = "";
+    
+    const inputChek= JSON.parse(input);
+    
+     if(Array.isArray(inputChek)){
+        out= "true";
+    }else{
+        out = "fulse";
+    }
+
+
+
+
+
+
+
+
+    output.innerText = out;
+}
